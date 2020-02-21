228a5af85e8a08ce501b0ab9e48ead7a252aab26
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-21 11:28:23 -0500

    made the call and got the data i needed and it renders to the screen

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios"
+import StarCard from './components/StarCard';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,23 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [people,setPeople]=useState([]);
+
+  useEffect(()=>{
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response =>{
+        setPeople(response.data.results)
+        console.log(response.data.results)
+    })
+  },[]);
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {people.map(person =>{
+        return <StarCard key={person.id}name={person.name}/>
+      })}
     </div>
   );
 }
